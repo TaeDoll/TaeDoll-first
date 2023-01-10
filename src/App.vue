b2e919528ca4f6baabc919c9b6173b721294d77b
TaeDol <zpakswkd@gmail.com>
2023-01-11 01:21:42 +0900

    v1

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar">
+    <div class="navbar__logo">
+      <i><font-awesome-icon :icon="['fas','camera-retro']"/></i>  
+      <a href="">태됴리 포트폴리오</a>
+    </div>
+    <ul class="navbar__menu">
+        <a href="">gallery</a>
+        <a href="">blog</a>
+        <a href="">about</a>
+        <a href="">notice</a>
+    </ul>
+    <ul class="navbar__links">
+        <i><font-awesome-icon :icon="['fab','instagram']"/>&nbsp;</i>
+         <i><font-awesome-icon :icon="['fas','heart']"/>&nbsp;</i>       
+        <i><font-awesome-icon :icon="['fab','github']"/></i>          
+    </ul>
+  </nav>
+  <img src="https://rtender.kr/web/upload/NNEditor/20221215/banafish20demi20yellow20film201350202.png"/>
+  
+
+
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+  @import "css/style.css";
+</style>
